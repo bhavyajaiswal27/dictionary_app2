e4ce08765196e61a78016f4225d31eb73f01ad84
Bhavya Jaiswal <jaiswalbhavya2005@gmail.com>
2024-06-18 09:15:03 +0530

    hello

---
+++ lib/homeScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('dictionay'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          
+        ),
+        ),
+    );
+  }
+}// import 'package:';
+
---
+++ lib/services.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:dictionary_app/dictionary_model.dart';
+
+class ApiServices {
+  String baseurl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+  Future <DictionaryModel?> fetchData (String word) async {
+    Uri url = Uri.parse("$baseurl$word");
+    final response = await http.get(url);
+    try {
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return DictionaryModel.fromJson(data[0]);
+      }
+      else {
+        throw("failed to load");
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  } 
+}
